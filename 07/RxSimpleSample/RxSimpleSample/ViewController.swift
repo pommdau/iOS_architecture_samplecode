c2629427b43a01a2229233256b3b45116c49b947
IKEH1024 <pondau1024@gmail.com>
2021-12-24 08:32:37 +0900

    reading 7章終わりまで

--- 07/RxSimpleSample/RxSimpleSample/ViewController.swift
+++ 07/RxSimpleSample/RxSimpleSample/ViewController.swift
@@ -15,6 +15,8 @@ final class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet private weak var passwordTextField: UITextField!
     @IBOutlet private weak var validationLabel: UILabel!
 
+    // RxではViewModelを作成する際に、UITextFieldのObservableを渡す
+    // UITextFieldのrx拡張からtextプロパティを取り出し、更にasObservable()でObservableを取り出し渡している
     private lazy var viewModel = ViewModel(
         idTextObservable: idTextField.rx.text.asObservable(),
         passwordTextObservable: passwordTextField.rx.text.asObservable(),
@@ -24,7 +26,9 @@ final class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // viewModel.validationTextとvalidationLabel.rx.textとをbind(to:)でデータバインディング
+        // これえviewModel.validationTextの変更に同期して、validationLabelの文字も変更されるようになる
         viewModel.validationText
             .bind(to: validationLabel.rx.text)
             .disposed(by: disposeBag)
@@ -33,7 +37,8 @@ final class ViewController: UIViewController, UITextFieldDelegate {
             .bind(to: loadLabelColor)
             .disposed(by: disposeBag)
     }
-
+    
+    // 色の更新処理をBinder化する
     private var loadLabelColor: Binder<UIColor> {
         return Binder(self) { me, color in
             me.validationLabel.textColor = color
